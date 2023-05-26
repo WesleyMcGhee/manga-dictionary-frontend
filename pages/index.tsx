5d011336b99edb1b7c424bcf8fc98f2ef6902aa0
Wesley <wesmcghee6372@gmail.com>
2023-05-26 10:54:34 -0500

    added comming soon page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import crying from "../public/cryingGirl.png";
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +10,13 @@ const Home: NextPage = () => {
       <Head>
         <title>Coming Soon</title>
       </Head>
-      <h1>Coming Soon</h1>
+      <h1 className={styles.comingSoonLogo}>Coming Soon</h1>
+      <p className={styles.comingSoonText}>
+        Don't worry it'll be here before you know it
+      </p>
+      <div className={styles.cryingContainer}>
+        <Image src={crying} height={336} width={223} alt="crying waifu" />
+      </div>
     </div>
   );
 };
